c1b6c5a487678da8683d81c1d17c45d33e564a89
白川義丸 <rannocat@yahoo.co.jp>
2015-05-24 14:37:07 +0900

    close #64

--- yottyan/iOS-client/My1DayApp/PostViewController.swift
+++ yottyan/iOS-client/My1DayApp/PostViewController.swift
@@ -14,8 +14,9 @@ protocol PostViewControllerDelagate : NSObjectProtocol {
 
 class PostViewController: UIViewController {
     @IBOutlet weak private var messageTextView: UITextView!
-    weak var delegate: PostViewControllerDelagate?
+        weak var delegate: PostViewControllerDelagate?
     // Mission1-2 Storyboard から UITextField のインスタンス変数を追加
+    @IBOutlet weak private var userNameTextView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +27,7 @@ class PostViewController: UIViewController {
     
     @IBAction func didTouchUpCloseButton(sender: AnyObject) {
         self.messageTextView.resignFirstResponder()
+        self.userNameTextView.resignFirstResponder()
         self.delegate?.postViewController(self, didTouchUpCloseButton: sender)
     }
     
@@ -33,10 +35,11 @@ class PostViewController: UIViewController {
         self.messageTextView.resignFirstResponder()
         
         let message: String = self.messageTextView.text ?? ""
+        let userName: String = self.userNameTextView.text ?? ""
         // Mission1-2 UITextField のインスタンス変数から値を取得
         
         // Mission1-2 posetMessage の第2引数に 任意の値を渡す
-        APIRequest.postMessage(message, username: "名前はまだない") {
+        APIRequest.postMessage(message, username: userName) {
             [weak self] (data, response, error) -> Void in
             
             self?.delegate?.postViewController(self!, didTouchUpCloseButton: sender)

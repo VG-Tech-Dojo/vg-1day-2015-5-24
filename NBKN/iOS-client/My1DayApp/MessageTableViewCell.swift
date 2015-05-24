253068155acad2a6a2c1116a5b021963570dea3b
NBKN <knt.watanab@gmail.com>
2015-05-24 14:15:56 +0900

    mission1.2 ユーザ名の投稿

--- NBKN/iOS-client/My1DayApp/MessageTableViewCell.swift
+++ NBKN/iOS-client/My1DayApp/MessageTableViewCell.swift
@@ -12,12 +12,15 @@ class MessageTableViewCell: UITableViewCell {
     @IBOutlet weak private var iconImageView: UIImageView!
     @IBOutlet weak private var messageLabel: UILabel!
     @IBOutlet weak private var dateLabel: UILabel!
+    @IBOutlet weak private var nameLabel: UILabel!
+
     // Mission1-1 UILabel のインスタンス変数を追加
     
     override func prepareForReuse() {
         self.iconImageView.image = nil
         self.messageLabel.text = nil
         self.dateLabel.text = nil
+        self.nameLabel.text = nil
         // Mission1-1 UILabel のインスタンス変数を初期化
     }
     
@@ -25,6 +28,7 @@ class MessageTableViewCell: UITableViewCell {
         self.iconImageView.image = message.icon
         self.messageLabel.text = message.body
         self.dateLabel.text = message.created_at
+        self.nameLabel.text = message.username
         // Mission1-1 UILabel のインスタンス変数に created_at の値を代入
     }
 }

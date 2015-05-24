3752535c8884587ad48ad8d4ae5fbb4b7fef5f35
shuhei <kawashu0723@gmail.com>
2015-05-24 15:14:31 +0900

    占いを追加

--- shuhei/web-server/app.php
+++ shuhei/web-server/app.php
@@ -23,9 +23,32 @@ $app->post('/messages', function (Request $request) use ($app) {
     $username = isset($data['username']) ? $data['username'] : '';
     $body = isset($data['body']) ? $data['body'] : '';
 
-    $createdMessage = $app->createMessage($username, $body, base64_encode(file_get_contents($app['icon_image_path'])));
-
-    $createdMessage = $app->createMessage("Bot", $body, base64_encode(file_get_contents($app['icon_image_path'])));
+    if ($data['body'] == "uranai") {
+      switch (mt_rand (0, 9)) {
+        case 0:
+        case 1:
+          $createdMessage = $app->createMessage("Bot_uranai", "daikichi", base64_encode(file_get_contents($app['icon_image_path'])));
+          break;
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+          $createdMessage = $app->createMessage("Bot_uranai", "kichi", base64_encode(file_get_contents($app['icon_image_path'])));
+          break;
+        case 8:
+        case 9:
+          $createdMessage = $app->createMessage("Bot_uranai", "kyou", base64_encode(file_get_contents($app['icon_image_path'])));
+          break;
+        default:
+          $createdMessage = $app->createMessage("Bot_uranai", "kichi", base64_encode(file_get_contents($app['icon_image_path'])));
+          break;
+        }
+    } else {
+      $createdMessage = $app->createMessage($username, $body, base64_encode(file_get_contents($app['icon_image_path'])));
+      $createdMessage = $app->createMessage("Bot", $body, base64_encode(file_get_contents($app['icon_image_path'])));
+    }
 
     return $app->json($createdMessage);
 });
